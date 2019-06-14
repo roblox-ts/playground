9488f182c1e8cd564bc1759849545a4ebb5cf646
osyrisrblx <osyrisrblx@gmail.com>
2019-06-14 14:30:39 -0700

    Updates

--- rbxts.js
+++ rbxts.js
@@ -4,3 +4,7 @@ const project = new Project();
 global.compileSource = source => {
 	return project.compileSource(source);
 };
+
+global.registerLib = (name, source) => {
+	project.project.createSourceFile(name, source);
+};
